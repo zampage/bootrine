075f94a05e93a460e245106fbde8018124d4a002
Markus Chiarot <cmarkus@hotmail.ch>
2016-05-09 16:09:42 +0200

    login designed

---
+++ view/pages/login.php
@@ -0,0 +1,16 @@
+<div class="outer-wrapper">
+	<div class="page-header">
+		<h1>Login</h1>
+	</div>
+	<form id="login-form" action="" method="POST">
+		<div class="input-group">
+			<span class="input-group-addon glyphicon glyphicon-user"></span>
+			<input type="text" class="form-control" placeholder="Benutzername">
+		</div>
+		<div class="input-group">
+			<span class="input-group-addon glyphicon glyphicon-lock"></span>
+			<input type="password" class="form-control" placeholder="Passwort">
+		</div>
+		<input type="submit" class="btn btn-default">
+	</form>
+</div>
